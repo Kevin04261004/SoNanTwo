c5d617cb95bdfae65339c7716beef5716c9d544b
Leetaro <47050040+Leetaro@users.noreply.github.com>
2023-11-18 19:21:35 +0900

    우클릭 누르고 있을 시 그 쪽봄
    god ju hwan

--- Assets/01_Scripts/Player/SkillShooter.cs
+++ Assets/01_Scripts/Player/SkillShooter.cs
@@ -25,10 +25,29 @@ public class SkillShooter : MonoBehaviour
     {
         if (PV.IsMine)
         {
-            if (Input.GetMouseButtonDown(1))
+            if (Input.GetMouseButtonUp(1))
             {
                 UseSkill();
-            }   
+            }
+            if (Input.GetMouseButton(1))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                Vector3 m_dir = Vector3.zero;
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    m_dir = (hit.point - transform.position).normalized;
+                    transform.rotation = Quaternion.LookRotation(new Vector3(m_dir.x, 0, m_dir.z)); // 신 주 환
+                }
+
+                // ==== 실 패 작 ====
+                //transform.forward = new Vector3(hit.point.x, hit.point.y, hit.point.z) - transform.position;
+                //transform.rotation = Quaternion.FromToRotation(transform.forward, new Vector3(hit.point.x, 0, hit.point.z));
+                //Vector3 dir = GetMousePositon() - transform.position;
+                //transform.rotation = Quaternion.Euler(0, Mathf.Atan2(dir.x, dir.) * Mathf.Rad2Deg, 0);
+                //Quaternion.Euler(0, Quaternion.FromToRotation(Vector3.forward, transform.position - GetMousePositon()).eulerAngles.z, 0);
+                // ==================
+            }
         }
     }
 
@@ -40,8 +59,15 @@ public class SkillShooter : MonoBehaviour
                 PhotonNetwork.Instantiate(redBullet_Prefab.name, transform.position, transform.rotation);
                 break;
             default:
-                Debug.Assert(true,"default bug!!!");
+                Debug.Assert(true, "default bug!!!");
                 break;
         }
     }
+
+    // 마우스 위치 구하는 거였으나 버려짐
+    //private Vector3 GetMousePositon()
+    //{
+    //    Vector3 point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+    //    return point;
+    //}
 }
