5ca9cfa07d7597a508ab83073c4041b9f6b94884
Choi Seung Ik <krppbud12@naver.com>
2023-11-18 19:58:44 +0900

    Random Box Create

---
+++ Assets/01_Scripts/Block/BlockSpawnManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+enum BLOCK
+{
+    low,
+    normal,
+    high,
+
+}
+
+public class BlockSpawnManager : MonoBehaviourPun
+{
+    public GameObject target;
+    public GameObject[] Block;
+
+    public int numberOfObject;
+
+    private void Start()
+    {
+        for(int i = 0; i < numberOfObject; i++)
+        {
+            Spawnobjects();
+            //CreateBlock();
+            /*Vector3 randomPosition = GetNonoverlapPos();
+            GameObject clone = Instantiate(Block[Random.Range(0, Block.Length)], randomPosition, Quaternion.identity);*/
+        }
+    }
+
+    private void Spawnobjects()
+    {
+        bool b = true;
+        while(b)
+        {
+            float xPos = Random.Range(-18f, 18f);
+            float yPos = 1f;
+            float zPos = Random.Range(-18f, 18f);
+            Vector3 spawnPos = new Vector3(xPos, yPos, zPos);
+            if (FindCollisions(spawnPos) < 1)
+            {
+                GameObject c = Instantiate(Block[Random.Range(0, Block.Length)], spawnPos, Quaternion.identity);
+                b = false;
+            }
+        }
+
+    }
+
+    private int FindCollisions(Vector3 pos)
+    {
+        Collider[] hits = Physics.OverlapSphere(pos, 0.5f);
+        for(int i = 0; i < hits.Length; i++)
+        {
+            if(hits[i].CompareTag("Wall"))
+            {
+                return 2;
+            }
+        }
+        return 0;
+    }
+
+    private void CreateBlock()
+    {
+        GameObject block = Block[Random.Range(0, Block.Length)];
+
+        Vector3 randomPosition = new Vector3(Random.Range(-18f, 18f), 1f, Random.Range(-18f, 18f));
+        int LoopStop = 0;
+        while (Physics.CheckSphere(randomPosition, 0.5f))
+        {
+            LoopStop++;
+            if (LoopStop > 10000)
+            {
+                Debug.Log("Something Wrong Box Position");
+                break;
+            }
+            randomPosition = new Vector3(Random.Range(-18f, 18f), 1f, Random.Range(-18f, 18f));
+        }
+        if(LoopStop < 10000)
+        {
+            Instantiate(block, randomPosition, Quaternion.identity);
+            //PhotonNetwork.Instantiate(block.name, randomPosition, Quaternion.identity);
+        }
+
+    }
+}
